1163e94567eeb87fe68826c2a03308f01149523f
Martin <20106234@mail.wit.ie>
2024-03-01 18:56:35 +0100

    Add draft dynamo db table

--- lib/app-api.ts
+++ lib/app-api.ts
@@ -2,6 +2,7 @@ import { Aws } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as apig from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as node from "aws-cdk-lib/aws-lambda-nodejs";
 
@@ -14,6 +15,32 @@ export class AppApi extends Construct {
   constructor(scope: Construct, id: string, props: AppApiProps) {
     super(scope, id);
 
+    // MovieId (Partition key) - Number
+    // ReviewerName - String (a username).
+    // ReviewDate - String, e.g. "2023-10-20".
+    // Content - String (the review text).
+    // Rating - Number (integer; range 1-5)
+
+    // Table
+    const movieReviewsTable = new dynamodb.Table(this, "MovieReviewsTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "movieId", type: dynamodb.AttributeType.NUMBER },
+      sortKey: { name: "reviewerName", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "MovieReviews",
+    });
+
+    movieReviewsTable.addLocalSecondaryIndex({
+      indexName: "roleIx",
+      sortKey: { name: "reviewDate", type: dynamodb.AttributeType.STRING },
+    });
+
+    movieReviewsTable.addLocalSecondaryIndex({
+      indexName: "roleIx",
+      sortKey: { name: "rating", type: dynamodb.AttributeType.NUMBER },
+    });
+
+    // REST API
     const appApi = new apig.RestApi(this, "AppApi", {
       description: "App RestApi",
       endpointTypes: [apig.EndpointType.REGIONAL],

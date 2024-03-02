45ee66331cd0dd12edf8da05e2d565e54e1febaa
Martin <20106234@mail.wit.ie>
2024-03-02 10:58:20 +0100

    Add movie id parameter and return 404 if parameter not valid

--- lambdas/getReviewsByMovieId.ts
+++ lambdas/getReviewsByMovieId.ts
@@ -1,8 +1,34 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 export const handler : APIGatewayProxyHandlerV2  = async function (event: any) {
-	return {
-		statusCode: 200,
-		body: 'This is the getReviewsByMovieId lambda',
-	};
+    try {
+        console.log("Event: ", event);
+        const parameters  = event?.pathParameters;
+
+        const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+
+        if (!movieId) {
+            return {
+              statusCode: 404,
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify({ Message: "Missing movie Id" }),
+            };
+        }
+
+        return {
+            statusCode: 200,
+            body: `This is the getReviewsByMovieId lambda, the movieId parameter is ${movieId}`,
+        };
+    } catch (error: any) {
+        console.log(JSON.stringify(error));
+        return {
+        statusCode: 500,
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({ error }),
+        };
+    }
 };
